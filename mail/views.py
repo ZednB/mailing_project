7a4789ca52ab57d22600312cca7fb0adc7bda805
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-04-11 14:55:20 +0300

    Starting cronjob

--- mail/views.py
+++ mail/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
@@ -30,22 +32,30 @@ def send_news(request):
     if request.method == 'POST':
         email = request.POST['email']
         try:
-            client = Client.objects.get(email=email)
-            message = Message.objects.all()
-            log = Log.object.all()
-            status = log.status_try
-            response = log.response
-            theme = message.theme
-            body = message.body
-            send_mail(
-                subject=theme,
-                message=body,
-                from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[client.email]
-            )
-            return JsonResponse({'status': status, 'message': response})
+            newsletter = NewsLetter.objects.get(email=email)
+            current_time = datetime.datetime.now().time()
+            start_time = newsletter.start_time
+            end_time = newsletter.end_time
+            if start_time <= current_time <= end_time:
+                clients = Client.objects.filter(sending_settings=newsletter)
+                for client in clients:
+                    message = Message.objects.all()
+                    log = Log.object.all()
+                    status = log.status_try
+                    response = log.response
+                    theme = message.theme
+                    body = message.body
+                    send_mail(
+                        subject=theme,
+                        message=body,
+                        from_email=settings.EMAIL_HOST_USER,
+                        recipient_list=[client.email]
+                    )
+                    return JsonResponse({'status': status, 'message': response})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Sending time not within the specified period'})
         except ObjectDoesNotExist:
-            return JsonResponse({'status': 'error', 'message': 'Клиент с таким email не найден'})
+            return JsonResponse({'status': 'error', 'message': 'Sending settings not found'})
         except Exception as e:
             return JsonResponse({'status': 'error', 'message': str(e)})
     else:
