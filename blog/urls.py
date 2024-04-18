094c20d7271f47bae72fea0a54cb0b25910afb85
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-04-18 14:34:54 +0300

    Starting blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import BlogCreateView, BlogListView, BlogDetailView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('blog_create/', BlogCreateView.as_view(), name='blog_create'),
     path('blog_list/', BlogListView.as_view(), name='blog_list'),
     path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog_update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog_delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from blog.forms import BlogForm
 from blog.models import Blog
 from django.urls import reverse_lazy
 from pytils.translit import slugify
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(PermissionRequiredMixin, CreateView):
     model = Blog
-    form_class = BlogForm
+    permission_required = 'blog.add_blog'
+    fields = ('title', 'content', 'image', 'is_published')
     success_url = reverse_lazy('blog_list')
 
     def form_valid(self, form):
@@ -32,3 +34,23 @@ class BlogDetailView(DetailView):
         self.object.view_count += 1
         self.object.save()
         return self.object
+
+
+class BlogUpdateView(PermissionRequiredMixin, UpdateView):
+    model = Blog
+    permission_required = 'blog.change_blog'
+    fields = ('title', 'content', 'image', 'is_published')
+    success_url = reverse_lazy('blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+
+class BlogDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Blog
+    permission_required = 'blog.delete_blog'
+    success_url = reverse_lazy('blog_list')
